6281af486364bfdfff313a5c2e4fe2a29c74755c
Natan oliveira <natanb26@hotmail.com>
2022-01-31 14:58:20 -0300

    Construção de calculo de tangente e adição em fonte principal

--- GeometryAPP/Program.cs
+++ GeometryAPP/Program.cs
@@ -1,5 +1,6 @@
-﻿    using GeometryAPP.Context;
+﻿using GeometryAPP.Context;
 using GeometryAPP.Model;
+using GeometryAPP.Service;
 using System;
 
 namespace GeometryAPP
@@ -8,6 +9,7 @@ namespace GeometryAPP
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("########################################################################");
             Console.WriteLine("###                    Ultimate Master Calculator                    ###");
             Console.WriteLine("###                                                                  ###");
@@ -21,30 +23,37 @@ namespace GeometryAPP
             Console.WriteLine("########################################################################");
 
             Console.ReadKey();
+            
+            Line lLine = new();
+
             var lSquare1 = new FullSquare();
             var lSquare2 = new FullSquare();
+            LineService _LineSerivce = new();
             try
             {
+                Console.WriteLine("###                     Primeiro Quadrado                            ###");
+                LerQuadrado(lSquare1);
+                Console.WriteLine("###                      Segundo Quadrado                            ###");
+                LerQuadrado(lSquare2);
+                lSquare2.StartX = Convert.ToInt64(Console.ReadLine());
 
+                using (var db = new GeoContext())
+                {
+                    db.Square.AddAsync(lSquare1);
+                    db.Square.AddAsync(lSquare2);
+                    db.SaveChangesAsync();
+                }
 
+                lSquare1.SetPoints();
+                lSquare2.SetPoints();
 
-            Console.WriteLine("###                     Primeiro Quadrado                            ###");
-            LerQuadrado(lSquare1);
-            Console.WriteLine("###                      Segundo Quadrado                            ###");
-            LerQuadrado(lSquare2);
-            lSquare2.StartX = Convert.ToInt64(Console.ReadLine());
-            
-            using (var db = new GeoContext())
-            {
-                db.Square.AddAsync(lSquare1);
-                db.Square.AddAsync(lSquare2);
-                db.SaveChangesAsync();
+                var lTan = _LineSerivce.CalcTang(lSquare1.PC, lSquare2.PC);
+                
+
+                Console.WriteLine("Parabéns, o processamento acabou, agora vá buscar outro café...");
+                Console.ReadKey();
             }
-
-            Console.WriteLine("Parabéns, o processamento acabou, agora vá buscar outro café...");
-            Console.ReadKey();
-            }
-            catch( Exception e)
+            catch (Exception e)
             {
                 Console.WriteLine("Ocorreram erros durante o processamento: \r\n" + e.Message);
             }
---
+++ GeometryAPP/Service/LineService.cs
@@ -0,0 +1,27 @@
+﻿using GeometryAPP.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryAPP.Service
+{
+    public class LineService
+    {
+        public float CalcTang(Point a, Point b)
+        {
+            var lX = a.X - b.X;
+            var lY = a.Y - b.Y;
+
+            return lY / lX;
+        }
+
+        public Point GetFinalPoint (Square pSquare, float pTang, bool pDirection)
+        {
+            var Result = new Point();
+
+            return 
+        }
+    }
+}
